64e62336cc57342735407f83382f91dc179fc248
Gabriel Antony <gmamanican@unsa.edu.pe>
2025-08-27 19:56:24 -0500

    feat: add webinar details and image; update CourseModules layout with clsx

--- src/modules/landing/econix/LandingEconix.tsx
+++ src/modules/landing/econix/LandingEconix.tsx
@@ -11,6 +11,7 @@ import Footer from "../shared/components/Footer";
 import { WhatsAppCTAMultiple } from "../shared/components/WhatsAppCTAMultiple";
 import { ListWebinar } from "./components/ListWebinar";
 import webinar1 from "../../../assets/webinar1.jpg";
+import webinar2 from "../../../assets/webinar2.jpg";
 import { ListCourse } from "./components/ListCourse";
 
 const phinixSocialLinks = {
@@ -40,6 +41,26 @@ const webinars: WebinarItemContent[] = [
     extraNotes:
       "Además, conoce ECONIX, nuestra nueva división educativa con cursos en energía, sostenibilidad e innovación.",
     imgSrc: webinar1,
+  },
+  {
+    title:
+      "🌱✨ Webinar: Hidrógeno verde: conceptos clave, producción y seguridad para la sostenibilidad",
+    date: "Jueves 28 de agosto",
+    time: "7:00 p.m. (hora Perú)",
+    description:
+      "Participa en el webinar gratuito sobre Hidrógeno Verde y entérate cómo esta tecnología impulsa la sostenibilidad y las energías renovables. Además, accede al concurso de becas y semibecas para el Curso Especializado en Hidrógeno Verde.",
+    speaker: {
+      name: "Ing. José Sarachaga",
+      title: "Gerente de operaciones en ALS Life Sciences",
+      bio: "Ingeniero con más de 15 años de experiencia en minería, hidrocarburos e industria. Especialista en gestión ambiental y proyectos sostenibles, experto en hidrógeno verde y CCS.",
+    },
+    link: "https://lu.ma/4iigyt0p",
+    isFree: true,
+    platform: "Google Meet",
+    organizer: "Curso Especializado en Hidrógeno Verde",
+    extraNotes:
+      "Al inscribirte y asistir, podrás participar en el sorteo de becas y semibecas 🎓.",
+    imgSrc: webinar2,
   },
 ];
 const customContacts: Contact[] = [
